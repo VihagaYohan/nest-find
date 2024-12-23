480cd367f05a8e19d0de981b59dc11cba7c6258b
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2024-12-23 10:39:12 +0530

    Changes - Adding user authentication

--- app/(root)/sign-in.tsx
+++ app/(root)/sign-in.tsx
@@ -1,4 +1,11 @@
-import { View, Text, ScrollView, Image, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  Alert,
+} from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -6,8 +13,19 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import images from "@/constants/images";
 import icons from "@/constants/icons";
 
+// services
+import { login } from "@/lib/appwrite";
+
 const SignIn = () => {
-  const handleOnPress = () => {};
+  const handleOnPress = async () => {
+    const result = await login();
+
+    if (result) {
+      console.log("login success");
+    } else {
+      Alert.alert("Login failed");
+    }
+  };
 
   return (
     <SafeAreaView className="bg-white h-full">
